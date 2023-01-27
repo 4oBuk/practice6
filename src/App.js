b5ff90410d4a8170c8f62912e03c12399de6c319
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2023-01-27 11:42:04 +0200

    downgradle to react 16.8.0

--- src/App.js
+++ src/App.js
@@ -1,38 +1,52 @@
 import CalButton from "./CalculatorButton";
-import React, { useRef } from "react";
-function App() {
-  const calculatorInput = useRef(null);
-  const onButtonClick = (e) => {
-    console.log(e);
-    console.log(calculatorInput.current.value);
-    console.log(e.target.textContent);
-    calculatorInput.current.value += e.target.textContent;
-  };
-  return (
-    <>
-      <input readOnly={true} ref={calculatorInput} type="text" />
-      <CalButton value="=" action={onButtonClick} />
-      <br />
-      <CalButton value="+" action={onButtonClick}/>
-      <CalButton value="-" action={onButtonClick}/>
-      <CalButton value="/" action={onButtonClick}/>
-      <CalButton value="*" action={onButtonClick}/>
-      <br />
-      <CalButton value="0" action={onButtonClick}/>
-      <CalButton value="1" action={onButtonClick}/>
-      <CalButton value="2" action={onButtonClick}/>
-      <br />
-      <CalButton value="3" action={onButtonClick}/>
-      <CalButton value="4" action={onButtonClick}/>
-      <CalButton value="5" action={onButtonClick}/>
-      <br />
-      <CalButton value="6" action={onButtonClick}/>
-      <CalButton value="7" action={onButtonClick}/>
-      <CalButton value="8" action={onButtonClick}/>
-      <br />
-      <CalButton value="0" action={onButtonClick}/>
-    </>
-  );
+import React from "react";
+
+const onButtonClick = (e) => {
+  console.log(e);
+  // calculatorInput.current.value += e.target.textContent;
+};
+class App extends React.Component {
+  // TODO
+  // 1. check if the expession is valid
+  // 1.1. parse buffer
+  // 2. if valid calculate expression
+  // 2.1. parse buffer and calculate
+  // 3. clean buffer and write operation in history
+  // TODO
+  // 1. operator '-' can be on the beggining of the buffer
+  // 2. if expression is valid and a user press any operator
+  //  calculate value, clean bugger, add result and operator to the buffer
+  // 3. if expression isn't valid and previous element is operator
+  //    replace operator with new one
+  //
+  // };
+  render() {
+    return (
+      <>
+        <input readOnly={true} /*ref={calculatorInput}*/ type="text" />
+        <CalButton value="=" action={onButtonClick} />
+        <br />
+        <CalButton value="+" action={onButtonClick} />
+        <CalButton value="-" action={onButtonClick} />
+        <CalButton value="/" action={onButtonClick} />
+        <CalButton value="*" action={onButtonClick} />
+        <br />
+        <CalButton value="0" action={onButtonClick} />
+        <CalButton value="1" action={onButtonClick} />
+        <CalButton value="2" action={onButtonClick} />
+        <br />
+        <CalButton value="3" action={onButtonClick} />
+        <CalButton value="4" action={onButtonClick} />
+        <CalButton value="5" action={onButtonClick} />
+        <br />
+        <CalButton value="6" action={onButtonClick} />
+        <CalButton value="7" action={onButtonClick} />
+        <CalButton value="8" action={onButtonClick} />
+        <br />
+        <CalButton value="0" action={onButtonClick} />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/CalculatorButton.jsx
+++ src/CalculatorButton.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const CalculatorButton = ({ value, action }) => {
-  return <button onClick={action}>{value}</button>;
-};
+class CalculatorButton extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      value: props.value,
+      action: props.action,
+    };
+  }
+
+  render() {
+    const { value, action } = this.state;
+    return <button onClick={action}>{value}</button>;
+  }
+}
+// const CalculatorButton = ({ value, action }) => {
+// };
 
 export default CalculatorButton;
