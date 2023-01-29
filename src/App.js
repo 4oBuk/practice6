124b4c42998c32a4c9e1d204e5735a11b0467a12
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2023-01-29 11:40:00 +0200

    add regex for expression

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
 import CalButton from "./CalculatorButton";
 import React from "react";
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputValue: "test"
-    }
-  };
+      inputValue: "",
+      currentResult: 0,
+      history: "history:",
+    };
+  }
 
-  onButtonClick = (event) => {
+  numberClick = (event) => {
     let test = this.state.inputValue + event.target.textContent;
     this.setState({
-      inputValue : test
-    })
+      inputValue: test,
+      operatorAllowed: true,
+    });
+  };
+
+  calculate = (event) => {
+    const inputValue = this.state.inputValue;
+    let history = this.state.history;
+    // calcuate value
+    let result = 0;
+    let expression = `\n--------------\n ${inputValue}=${result}`;
+    this.setState({
+      history: history + expression,
+      inputValue: result,
+      operatorAllowed: false,
+    });
+  };
+
+  operatorClick = (event) => {
+    console.log("operator clicked");
+    const { inputValue } = this.state;
+    if (this.isExpressionValid(inputValue)) {
+      this.calculate();
+      const { inputAfterUpdate } = this.state;
+      this.setState({
+        inputValue: `${inputAfterUpdate}${event.target.textContent}`,
+      });
+      // get state of input again
+      // add operator to the end
+    } else {
+      if (inputValue === "") {
+        return;
+      }
+      const regex = new RegExp("[123456890]+");
+      if (regex.test(inputValue)) {
+        this.setState({
+          inputValue: `${inputValue}${event.target.textContent}`,
+        });
+      }
+      // check if there is number before
+      // add operator to the end
+    }
+  };
+  isExpressionValid(expession) {
+    const regex = /\d+[/*+-]{1}\d+$/;
+    return regex.test(expession);
+    // use regex to check it
+    // example 1: "7*1" -- valid
+    // example 2: "4*-5" -- not valid
   }
   // TODO
   // 1. check if the expession is valid
@@ -30,48 +78,47 @@ class App extends React.Component {
   //    replace operator with new one
   //
   // };
+
+  // !I have to check what to input on button click
+  // ? I can create another component or a function for numbers
+  // ? for example I will check is operator may be used for operation on click function
+  //    If I will allow to use negative nubmers I need a delete button or just only positive
+  // ? for numbers I will parse number and put it in state as current value
+  //
+
   render() {
+    const history = this.state.history;
     const inputValue = this.state.inputValue;
     return (
       <>
-        <input readOnly={true} value={inputValue} type="text" />
-        <CalButton value="=" action={this.onButtonClick} />
+        <div>
+          <textarea value={history} readOnly={true} />
+          <br />
+          <input readOnly={true} value={inputValue} type="text" />
+          <CalButton value="=" action={this.calculate} />
+        </div>
+        <div>
+          <CalButton value="+" action={this.operatorClick} />
+          <CalButton value="-" action={this.operatorClick} />
+          <CalButton value="/" action={this.operatorClick} />
+          <CalButton value="*" action={this.operatorClick} />
+        </div>
+        <CalButton value="0" action={this.numberClick} />
+        <CalButton value="1" action={this.numberClick} />
+        <CalButton value="2" action={this.numberClick} />
         <br />
-        <CalButton value="+" action={this.onButtonClick} />
-        <CalButton value="-" action={this.onButtonClick} />
-        <CalButton value="/" action={this.onButtonClick} />
-        <CalButton value="*" action={this.onButtonClick} />
+        <CalButton value="3" action={this.numberClick} />
+        <CalButton value="4" action={this.numberClick} />
+        <CalButton value="5" action={this.numberClick} />
         <br />
-        <CalButton value="0" action={this.onButtonClick} />
-        <CalButton value="1" action={this.onButtonClick} />
-        <CalButton value="2" action={this.onButtonClick} />
+        <CalButton value="6" action={this.numberClick} />
+        <CalButton value="7" action={this.numberClick} />
+        <CalButton value="8" action={this.numberClick} />
         <br />
-        <CalButton value="3" action={this.onButtonClick} />
-        <CalButton value="4" action={this.onButtonClick} />
-        <CalButton value="5" action={this.onButtonClick} />
-        <br />
-        <CalButton value="6" action={this.onButtonClick} />
-        <CalButton value="7" action={this.onButtonClick} />
-        <CalButton value="8" action={this.onButtonClick} />
-        <br />
-        <CalButton value="0" action={this.onButtonClick} />
+        <CalButton value="0" action={this.numberClick} />
       </>
     );
   }
-}
-
-function calculate(expression) {
-  // example expression d+d
-  // how to parse ('-'oneOrZeroNumberOperatorNumber)
-  // parse two numbers and operator
-  // I can use switch to calculate value (if "+" sum)
-  // put expression in history and put result in input
-}
-
-function isExpressionValid(expession) {
-  // use regex to check it
-  // example 1: "7*1" -- valid
-  // example 2: "4*-5" -- not valid 
 }
 
 export default App;
